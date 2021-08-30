bc03e77b32f40f607d09ec518dfd8704a9b11037
CALEB VERMA <halasmish@gmail.com>
2021-08-30 02:14:57 -0700

    implement weeknd easter egg

--- index.js
+++ index.js
@@ -1,50 +1,45 @@
 $(document).ready(function () {
     // Assign keycodes to named variables
     // Bongo
-    var leftArrow = 37;
-    var rightArrow = 39;
+    var LEFT_ARROW = 37;
+    var RIGHT_ARROW = 39;
     // Synth
-    var keyQ = 81;
-    var keyW = 87;
-    var keyE = 69;
-    var keyR = 82;
+    var KEY_Q = 81;
+    var KEY_W = 87;
+    var KEY_E = 69;
+    var KEY_R = 82;
 
-    var keyU = 85;
-    var keyI = 73;
-    var keyO = 79;
-    var keyP = 80;
+    var KEY_U = 85;
+    var KEY_I = 73;
+    var KEY_O = 79;
+    var KEY_P = 80;
     // Sing
-    var keyA = 65;
-    var keyS = 83;
-    var keyD = 68;
-    var keyF = 70;
+    var KEY_A = 65;
+    var KEY_S = 83;
+    var KEY_D = 68;
+    var KEY_F = 70;
 
-    var keyJ = 74;
-    var keyK = 75;
-    var keyL = 76;
-    var semicolon = 186;
+    var KEY_J = 74;
+    var KEY_K = 75;
+    var KEY_L = 76;
+    var SEMICOLON = 186;
 
-    // Confetti counter
-    var confettiCounter = 0;
-
+    var totalConfetti = 0;
+    var confettiPopped = 0;
+    
     // Make cat confetti rain down from random x-axis locations at random delays
     $("#cat-confetti-container").children().each(function () {
         $(this).css("left", `${Math.floor(Math.random() * 90)}vw`)
         $(this).css("animation-delay", `${Math.random() * 10}s`)
-        confettiCounter++;
+        totalConfetti++;
     });
-
-    // Confetti popped counter
-    var confettiPopped = 0;
 
     // Confetti pops and disappear when clicked
     $(".cat-confetti").click(function () {
         $(this).css("display", "none");
         new Audio("assets/sounds/meow.mp3").play();
-
-        // Display Easter Egg instructions if all confetti is popped
         confettiPopped++
-        if (confettiPopped >= confettiCounter) {
+        if (confettiPopped >= totalConfetti) {
             $("#easter-egg-instructions").toggle();
         }
     });
@@ -53,10 +48,7 @@ $(document).ready(function () {
     $("#play-button").click(function () {
         $("#welcome-message-screen-container").toggle();
         $("#bongo-cat-screen-container").toggle();
-        const playButtonSound = new Audio("assets/sounds/squeak.mp3");
-        playButtonSound.play();
-
-        // Ensures that only the bongo play buttons are visible
+        new Audio("assets/sounds/squeak.mp3").play();
         $("#change-to-bongo").click();
     });
 
@@ -143,9 +135,9 @@ $(document).ready(function () {
     };
 
     // Functions for playing instrument with keys
-    var instrumentKeys = [leftArrow, rightArrow, keyQ, keyW, keyE, keyR, keyU, keyI, keyO, keyP, keyA, keyS, keyD, keyF, keyJ, keyK, keyL, semicolon];
+    var instrumentKeys = [LEFT_ARROW, RIGHT_ARROW, KEY_Q, KEY_W, KEY_E, KEY_R, KEY_U, KEY_I, KEY_O, KEY_P, KEY_A, KEY_S, KEY_D, KEY_F, KEY_J, KEY_K, KEY_L, SEMICOLON];
     var instrumentNotes = ["bongo-left", "bongo-right", "synth-a", "synth-b", "synth-c", "synth-d", "synth-e-flat", "synth-e", "synth-f", "synth-g", "sing-do", "sing-re", "sing-mi", "sing-fa", "sing-so", "sing-la", "sing-ti", "sing-do-high"];
-    var isDown = {leftArrow: false, rightArrow: false, keyQ: false, keyW: false, keyE: false, keyR: false, keyU: false, keyI: false, keyO: false, keyP: false, keyA: false, keyS: false, keyD: false, keyF: false, keyJ: false, keyK: false, keyL: false, semicolon: false}
+    var isDown = {LEFT_ARROW: false, RIGHT_ARROW: false, KEY_Q: false, KEY_W: false, KEY_E: false, KEY_R: false, KEY_U: false, KEY_I: false, KEY_O: false, KEY_P: false, KEY_A: false, KEY_S: false, KEY_D: false, KEY_F: false, KEY_J: false, KEY_K: false, KEY_L: false, SEMICOLON: false }
 
     for (let i = 0; i < instrumentKeys.length; i++) {
         $(document).keydown(function (event) {
@@ -211,26 +203,48 @@ $(document).ready(function () {
 
     // Automatically switch to the correct instrument if a key corresponding to an instrument is pressed
     $(document).keydown(function (event) {
-        var bongoKeys = [leftArrow, rightArrow];
+        var bongoKeys = [LEFT_ARROW, RIGHT_ARROW];
         var keyCode = event.which;
         if (bongoKeys.includes(keyCode)) {
             $("#change-to-bongo").click();
-        } 
+        }
     });
 
     $(document).keydown(function (event) {
-        var synthKeys = [keyQ, keyW, keyE, keyR, keyU, keyI, keyO, keyP];
+        var synthKeys = [KEY_Q, KEY_W, KEY_E, KEY_R, KEY_U, KEY_I, KEY_O, KEY_P];
         var keyCode = event.which;
         if (synthKeys.includes(keyCode)) {
             $("#change-to-synth").click();
-        } 
+        }
     });
 
     $(document).keydown(function (event) {
-        var singKeys = [keyA, keyS, keyD, keyF, keyJ, keyK, keyL, semicolon];
+        var singKeys = [KEY_A, KEY_S, KEY_D, KEY_F, KEY_J, KEY_K, KEY_L, SEMICOLON];
         var keyCode = event.which;
         if (singKeys.includes(keyCode)) {
             $("#change-to-microphone").click();
-        } 
+        }
     });
+
+    var easterEggCodePosition = 0;
+    $("button").click(function (event) {
+        var easterEggCodeSequence = ["f", "f", "e-flat", "f", "g", "c", "e-flat"];
+        var easterEggComponent = `synth-${easterEggCodeSequence[easterEggCodePosition]}-play-button`;
+
+        if ($(this).attr("id") == easterEggComponent) {
+            easterEggCodePosition++;
+            if (easterEggCodePosition == easterEggCodeSequence.length) {
+                new Audio("assets/sounds/blinding-lights.mp3").play();
+                setTimeout(function(){
+                    $("#cat-no-hands").attr("src", "assets/pictures/weeknd-cat-no-hands.png");
+                    $("#cat-left-hand").attr("src", "assets/pictures/weeknd-cat-left-hand.png");
+                    $("#cat-right-hand").attr("src", "assets/pictures/weeknd-cat-right-hand.png");
+                    $("#cat-both-hands").attr("src", "assets/pictures/weeknd-cat-both-hands.png");
+                    $("#cat-singing").attr("src", "assets/pictures/weeknd-cat-singing.png");
+                }, 1000);
+                
+            }
+        }
+    });
+
 });
